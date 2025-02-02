974fb7f7e2750653fa89f9d30bbdaa658c0eac3b
Johan Boström <zarxor@users.noreply.github.com>
2025-02-02 09:40:24 +0100

    app array

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# setup-computer
+# Setup / Update computer
+
+## Linux
+Run command
+```
+curl -fsSL https://github.dev/zarxor/setup-computer/linux/install.sh | bash
+```
--- linux/install.sh
+++ linux/install.sh
@@ -3,21 +3,36 @@ if [ "${USER}" = "root" ]; then
     exit 0
 fi
 
+sudo apt-get update
+
+declare -a apps=(
+    "unzip"
+    )
+
+for app in "${apps[@]}"
+do
+    if dpkg -s $app &>/dev/null; then
+        echo "$app is installed"
+    else
+        sudo apt-get 
+    fi
+done
+
 if ! command -v oh-my-posh &>/dev/null; then
-    sudo apt-get update
     sudo apt-get -y install unzip
     sudo curl -s https://ohmyposh.dev/install.sh | bash -s
     sudo oh-my-posh font install Cousine
 fi
 
-if ! command -v volta &>/dev/null; then
-    curl https://get.volta.sh | bash
-fi
 
-if ! command -v yarn &>/dev/null; then
-    echo "Missing yarn"
+# if ! command -v volta &>/dev/null; then   
+#     curl https://get.volta.sh | bash
+# fi
+
+# if ! command -v yarn &>/dev/null; then
+#     echo "Missing yarn"
     
-fi
+# fi
 
 # if ! grep -q -F "oh-my-posh" "${HOME}/.bashrc"; then
 #     echo 'eval "$(oh-my-posh init bash --config /mnt/c/Users/johan/OneDrive/oh-my-posh.omp.json)"' >> "${HOME}/.bashrc"
